01edcb19b2357606c2f7ec82987a46d3f32877ea
Felipe Pereira Heitmann <felipeph@gmail.com>
2025-08-30 22:19:23 -0300

    comecando a trabalhar na logica dos tiles

--- game.py
+++ game.py
@@ -1,16 +1,30 @@
-
-TILE_SIZE = 64
-ROWS = 30
-COLS = 15
+# Constantes de tiles
+TILE_SIZE = 64 # Cada tijolinho tem 64 pixels
+ROWS = 15 # Quantidade de linhas
+COLS = 30 # Quantidade de colunas
 
 # Definir o tamanho e nome da janela
-WIDTH = TILE_SIZE * ROWS # Largura X
-HEIGHT = TILE_SIZE * COLS # Altura Y
+WIDTH = TILE_SIZE * COLS # Largura X
+HEIGHT = TILE_SIZE * ROWS # Altura Y
 TITLE = "Jogo de Plataforma" # Nome da Janela
 
 # Criando e posicionando nosso herói
 hero = Actor("hero")
 hero.pos = WIDTH / 2 , HEIGHT / 2 # Centro da Tela
+
+# Construir os elementos antes de desenhar
+def build(filename, tile_size):
+    # Abrindo o arquivo como leitura
+    with open(filename, "r") as f:
+        # Extraindo o conteúdo e quebrando linhas
+        contents = f.read().splitlines()
+    return contents
+
+# Lendo o arquivo das plataformas
+platforms = build("mapa_plataformas.csv", TILE_SIZE)
+
+# Imprimir o conteúdo das plataformas
+print(platforms)
 
 # Desenhar elementos na tela
 def draw():
@@ -21,5 +35,6 @@ def draw():
     # Desenhar o herói
     hero.draw()
 
+# Atualizar cada frame
 def update():
     pass
